9f1532e9c9dfc02b3bc2f568d162bb5645ae6c55
yivan-WYYGDSG <xieyufeng0119@outlook.com>
2021-04-23 20:58:04 +0800

    修复检测页码bug

--- util/footer.py
+++ util/footer.py
@@ -1,15 +1,18 @@
 # encoding: utf-8
 """
-@author: xie yufeng
+@author: duke mei
+@contact: mylovezju@163.com
 @software: PyCharm
-@file: footer.py
-@time: 2021/4/23 19:41
+@file: util.py
+@time: 2021/4/15 8:55
 """
-import os
-import fitz
+from os import remove
+from os.path import splitext
+import win32com.client
+from win32com.client import Dispatch, constants
 import docx
-
-from util import *
+import fitz
+import pydoc_data
 
 
 def find_page_number(pdf_file, keyword):
@@ -23,22 +26,23 @@ def find_page_number(pdf_file, keyword):
 
 
 def check_alignment(docx_file):
-    check_msg = []
     roma_nums_uppercase = ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X']
+    all_good = True
     document = docx.Document(docx_file)
     for section in document.sections:
         for parg in section.footer.paragraphs:
             if parg.text:
                 if parg.text in roma_nums_uppercase:
                     if str(parg.alignment) != 'CENTER (1)':
-                        # print('页码 {} 未居中'.format(parg.text))
-                        check_msg.append('页码 {} 未居中'.format(parg.text))
-    return check_msg
+                        print('页码 {} 未居中'.format(parg.text))
+                        all_good = False
+    if all_good:
+        print('目录页码已全部居中')
 
 
 def check_footer_nums(pdf_file, start_page, end_page):
     roma_nums_uppercase = ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X']
-    check_msg = []
+    all_good = True
     with fitz.open(pdf_file) as doc:
         for idx, page in enumerate(doc, start=1):
                 if start_page <= idx < end_page:
@@ -49,13 +53,13 @@ def check_footer_nums(pdf_file, start_page, end_page):
                         cur_sec = '表目录'
                     elif '目录' in page.getText():
                         cur_sec = '目录'
-                    if cur_footer in page.getText():
-                        # print("{} 页码：{}".format(cur_sec, cur_footer))
-                        check_msg.append("{} 页码：{}".format(cur_sec, cur_footer))
+                    data = [item.strip('\n') for item in page.getText().split(' ')]
+                    if cur_footer in data:
+                        print("{} 页码：{}".format(cur_sec, cur_footer))
                     else:
-                        # print("{} 缺少页码：{} 或者页码格式非大写罗马字符".format(cur_sec, cur_footer))
-                        check_msg.append("{} 缺少页码：{} 或者页码格式非大写罗马字符".format(cur_sec, cur_footer))
-    return check_msg
+                        print("{} 缺少页码：{} 或者页码格式非大写罗马字符".format(cur_sec, cur_footer))
+                        all_good = False
+    return all_good
 
 
 def check_footer(pdf, docx):
@@ -66,15 +70,22 @@ def check_footer(pdf, docx):
     :return: None
     '''
     stand_page_list = find_page_number(pdf, '目录')
-    p_list = find_page_number(pdf, '第 1 章')
-    footer_num_msg = check_footer_nums(pdf, min(stand_page_list), min(p_list))
-    alignment_msg = check_alignment(docx)
-    return footer_num_msg + alignment_msg
+    p_list = find_page_number(pdf, '第1章')
+    if check_footer_nums(pdf, min(stand_page_list), min(p_list)):
+        check_alignment(docx)
+    else:
+        print('请修改错误的页码')
+
+
+
+
 
 
 if __name__ == '__main__':
-    doc_path = os.getcwd() + '/../paper/硕士学位论文正文_1.doc'
-    pdf_path = doc_path.replace('doc', 'pdf')
-    docx_path = doc_path.replace('doc', 'docx')
-    msg = check_footer(pdf_path, docx_path)
-    print(msg)
+    # 这里只能使用完整绝对地址，相对地址找不到文件，且，只能用“\\”，不能用“/”，哪怕加了 r 也不行，涉及到将反斜杠看成转义字符。
+    doc_path = r'C:\\Users\\xieyu\\Desktop\\jupyter project\\论文课程作业\\test2.doc'
+    docx_path = r'C:\\Users\\xieyu\\Desktop\\jupyter project\\论文课程作业\\test2.docx'
+    pdf_path = r'C:\\Users\\xieyu\\Desktop\\jupyter project\\论文课程作业\\test2.pdf'
+    check_footer(pdf_path, docx_path)
+
+
